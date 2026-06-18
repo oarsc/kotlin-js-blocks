fabe4cd55da3c61bc67136e5a313adfea3bfc363
oarsc <osc.car2@gmail.com>
2026-06-18 10:21:08 +0200

    JitPack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
     id("org.jetbrains.kotlin.multiplatform") version "2.1.10"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
-    id("maven-publish")
 }
 
-group = "org.oar.lib"
+group = "com.github.oarsc"
 version = "1.0.0"
 
 repositories {
@@ -13,9 +12,7 @@ repositories {
 
 kotlin {
     js(IR) {
-        browser {
-            // No executable binaries here (this is a library)
-        }
+        browser()
     }
 
     sourceSets {
@@ -23,20 +20,6 @@ kotlin {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-            }
-        }
-    }
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/oarsc/kotlin-js-blocks")
-
-            credentials {
-                username = System.getenv("GITHUB_USER")
-                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
